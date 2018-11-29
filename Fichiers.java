564fd3fc706db20d5bfcf737275195f907f79146
Mat111293 <45318946+Mat111293@users.noreply.github.com>
2018-11-29 16:12:50 -0500

    Delete Fichiers.java

--- Fichiers.java
+++
@@ -1,9 +0,0 @@
-package ca.csf.dfc.Dessin.Repository;
-
-import java.util.Vector;
-
-public interface Fichiers extends IChargeable, ISauvegardable, IExportable {
-
-    public Vector  myControleurDessin;
-
-}
